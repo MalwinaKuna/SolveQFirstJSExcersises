cbedd7c67dc55bef1e3c2b1df96cb8c5d051bf92
Malwina Mieczkowska <malwina@mieczkowski.eu>
2019-07-03 15:44:14 +0200

    additional Task with test

--- src/Task.js
+++ src/Task.js
@@ -1,21 +1,21 @@
-class Task{
-    isItHot(temp){
-        if(temp>29){
+class Task {
+    isItHot(temp) {
+        if (temp > 29) {
             return true;
-        }else{
-            return false;}
+        } else {
+            return false;
+        }
     }
 
-    counter(number){
-        let count=0;
-        for(let i=0;i<number;i++){
-            count+=3;
-        } 
+    counter(number) {
+        let count = 0;
+        for (let i = 0; i < number; i++) {
+            count += 3;
+        }
         return count;
     }
-
-    add(a,b){
-        return a+b;
+    add(a, b) {
+        return a + b;
     }
 }
-module.exports=Task;
+module.exports = Task;
--- test/Task.test.js
+++ test/Task.test.js
@@ -1,16 +1,18 @@
-const Task=require('../src/Task');
+const Task = require('../src/Task');
 
-test('description',()=>{
-    let ob=new Task();
+test('description', () => {
+    let ob = new Task();
     expect(ob.isItHot(30)).toBe(true);
 })
-test('desc',()=>{
-    let obj1=new Task();
+test('desc', () => {
+    let obj1 = new Task();
     expect(obj1.counter(3)).toBe(9);
 })
 
-test('desc', ()=>{
 
-    let obj2= new Task();
-    expect(obj2.add(1,1)).toBe(2);
+
+test('desc', () => {
+
+    let obj2 = new Task();
+    expect(obj2.add(1, 1)).toBe(2);
 })
