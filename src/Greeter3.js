62b68eb3ec85496732ec53da3d963ff2039d19f2
Malwina Mieczkowska <malwina@mieczkowski.eu>
2019-07-08 15:25:21 +0200

    reformatted code and first test repaired

--- src/Greeter3.js
+++ src/Greeter3.js
@@ -1,15 +1,13 @@
 class Greeter3 {
-
     constructor(date) {
         this.date = date;
     }
-
     greeter(name) {
         let greet = 'Hello ';
         let hours = this.date.getHours();
         name.trim();
         name = name.charAt(0).toUpperCase() + name.slice(1);
-        if (hours >= 6 && hours <= 12) {
+        if (hours >= 6 && hours < 12) {
             greet = 'Good morning ';
         } else if (hours < 22 && hours >= 18) {
             greet = 'Good evening ';
@@ -17,10 +15,7 @@ class Greeter3 {
         } else if (hours >= 22 && hours < 24 || hours > 0 && hours < 6) {
             greet = 'Good night ';
         }
-
         return greet += name;
-
     }
-
 }
 module.exports = Greeter3;
